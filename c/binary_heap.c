c21e72cb4ab20ac3fc6fcd2e4c0e922e0aa391b1
Vishnu Goyal <blissvish@gmail.com>
2017-09-29 21:59:39 +0530

    Update binary_heap.c

--- c/binary_heap.c
+++ c/binary_heap.c
@@ -3,16 +3,20 @@
 #include<math.h>
 //included math.h for the ceil function
 
-int labour_max_heap(int*x, int element_count,int index){
-int local_max = index;
-int temp,right_index = 2*(1+index);int left_index = right_index-1;
+int labour_max_heap(int*x, int element_count,int index){ //classically known as max heapify
+int local_max = index; //temporary variable to store the index of max in the stack of a parent and it's two children
+	
+int temp,right_index = 2*(1+index); //the right child index in the array representation of binary heap
+int left_index = right_index-1; // the left child index in binary heap's array representation.
+	
 if((x[right_index] > x[index]) && (right_index <= element_count))
-	local_max = right_index;	
+	local_max = right_index; // if 	right child exists and is greater than the parent, local_max to hold the right child index
 if ((x[left_index] > x[local_max] ) && (left_index <= element_count))
-	local_max = left_index;
-if(local_max != index)
+	local_max = left_index; // if left index exists and is grater than the element at local_max index (right_child if the above if statement got executed else index)
+	//local_max gets updated
+if(local_max != index) // if local max has changed 
 {
-	temp = x[local_max];
+	temp = x[local_max]; //swap the parent child pair and recursively call the max_heapify on the child credentials- as a parent
 	x[local_max] = x[index];
 	x[index] = temp;
 	labour_max_heap (x,element_count,local_max); 
